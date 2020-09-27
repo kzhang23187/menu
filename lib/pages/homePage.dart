a2ea91721f3221d9f84196f97c2311cef8cbed49
Kevin Zhang <kzhang23187@gmail.com>
2020-09-27 01:11:17 -0500

    Added half of the home page. Need to be able to add from a selected recipe list.

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,30 +1,96 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import '../widgets/recipeListWidget.dart';
+
+List<String> dummy = [
+  "taco 1",
+  "taco 2",
+  "taco 3",
+  "taco 4",
+  "taco 5",
+  "taco 6",
+  "taco 7"
+];
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _controller = ScrollController();
+  bool newItemAdded = false;
+  void removeRecipe(String name) {
+    setState(() {
+      dummy.remove(name);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Timer(Duration(milliseconds: 500), () {
+      if (newItemAdded) {
+        _controller.animateTo(
+          _controller.position.maxScrollExtent,
+          duration: Duration(milliseconds: 500),
+          curve: Curves.fastLinearToSlowEaseIn,
+        );
+        setState(() {
+          newItemAdded = false;
+        });
+      } else {
+        _controller.animateTo(_controller.offset, duration: null, curve: null);
+      }
+    });
+
     return Scaffold(
       body: SafeArea(
         child: Column(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 20.0),
-              child: Text(
-                "Today's Meals",
-                textAlign: TextAlign.left,
-                style: TextStyle(fontSize: 30),
+            Container(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text(
+                  "Today's Meals",
+                  style: TextStyle(fontSize: 30),
+                ),
               ),
             ),
             Container(
-              height: MediaQuery.of(context).size.height / 2,
-              child: ListView(
-                children: <Widget>[
-                  Text("taco 1"),
-                  Text("Taco 2"),
-                  Text("Taco 3")
-                ],
+              height: 2 * MediaQuery.of(context).size.height / 5,
+              child: ListView.builder(
+                controller: _controller,
+                itemCount: dummy.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                      margin: const EdgeInsets.all(3.0),
+                      padding: const EdgeInsets.all(3.0),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          border: Border.all(color: Colors.blue, width: 2)),
+                      child: RecipeListItem(dummy[index], removeRecipe));
+                },
+                // separatorBuilder: (context, index) => Divider(),
               ),
-            )
+              // child: ListView(
+              //   children: <Widget>[
+              //     RecipeListItem(),
+              //     Text("Taco 2"),
+              //     Text("Taco 3")
+              //   ],
+              // ),
+            ),
+            Container(
+                child: IconButton(
+                    icon: Icon(Icons.add_circle),
+                    onPressed: () {
+                      setState(() {
+                        dummy.add("chicken");
+                        newItemAdded = true;
+                      });
+                    })),
           ],
         ),
       ),
--- lib/widgets/recipeListWidget.dart
+++ lib/widgets/recipeListWidget.dart
@@ -1,6 +1,29 @@
+import 'package:flutter/material.dart';
+
 class RecipeListItem extends StatelessWidget {
+  final String recipeName;
+  final removeRecipe;
+  RecipeListItem(this.recipeName, this.removeRecipe);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Checkbox(value: false, onChanged: null),
+        Flexible(
+          child: Text(
+            this.recipeName,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              removeRecipe(recipeName);
+            }),
+      ],
+    );
   }
 }
